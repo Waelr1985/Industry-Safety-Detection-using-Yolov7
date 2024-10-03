3de8eca6812da5b7ffe65172ce46fbb644fa6576
waelr1985 <waelr1985@gmail.com>
2024-10-04 01:59:24 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Wael Rahhal',
+    author_email= 'waelr1985@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
